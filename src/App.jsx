196a74b353389a58cdf8a30e9ced8e84e0f22a11
Emiliano Volpino <volpinoemiliano@gmail.com>
2023-10-07 19:20:05 -0300

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import react from 'react';
+import Navbar from './components/Navbar/Navbar.jsx';
+
+const App = () => {
+
+  return (
+    <>
+      <Navbar/>
+      <h1>Boeeenas</h1>
+    </>
+  )
+}
+
+export default App
