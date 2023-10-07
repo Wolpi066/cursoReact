93f3faf83a1228112d565b414c28900561181053
Emiliano Volpino <volpinoemiliano@gmail.com>
2023-10-07 20:11:30 -0300

    Segundo commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
-import react from 'react';
+import React from 'react';
 import Navbar from './components/Navbar/Navbar.jsx';
+import './components/Navbar/Navbar.css';
+import Sidebar from './components/Sidebar/Sidebar.jsx';
+import './components/Sidebar/Sidebar.css';
+import './App.css';
 
 const App = () => {
-
   return (
-    <>
-      <Navbar/>
-      <h1>Boeeenas</h1>
-    </>
+    <div>
+      <Navbar />
+      <Sidebar />
+    </div>
   )
 }
 
-export default App
+export default App;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,19 @@
-import React from "react";
+import React from 'react';
+import "./Navbar.css";
 
 const Navbar = () => {
-    return (
-        <h1>Navbar</h1>
-    )
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <a className="navbar-brand" href="/">
+        <img src="ruta-al-logo.png" alt="Logo" className="logo" />
+        Mi Título
+      </a>
+      <form className="form-inline ml-auto">
+        <input className="form-control" type="search" placeholder="Buscar..." aria-label="Search" />
+        <button className="btn btn-outline-primary" type="submit">Buscar</button>
+      </form>
+    </nav>
+  );
 }
+
+export default Navbar;
